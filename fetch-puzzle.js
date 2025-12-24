38400c1fc06bc16b78f9a74a533d6be0d68faa21
fenton-k <97002770+fenton-k@users.noreply.github.com>
2025-12-24 13:28:14 -0500

    Remove duplicate 'https' import
    Removed duplicate import of the 'https' module.

--- fetch-puzzle.js
+++ fetch-puzzle.js
@@ -1,8 +1,6 @@
 const fs = require("fs").promises;
 const https = require("https");
 
-const https = require("https");
-
 function fetchJson(url) {
   return new Promise((resolve, reject) => {
     const options = {
